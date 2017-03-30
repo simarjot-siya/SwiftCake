a9840c666c2a6ac9cd5f4de6f56ec9bb7eb668e3
Dominik Majda <ddmajda@gmail.com>
2017-03-30 14:06:41 +0200

    Reset to midnight - return fix

--- SwiftCake/Comparator.swift
+++ SwiftCake/Comparator.swift
@@ -39,7 +39,7 @@ extension Date {
     
     public func resetToMidnight() -> Date {
         let calendar = Calendar.current
-        calendar.startOfDay(for: self)
+        return calendar.startOfDay(for: self)
     }
     
 }
